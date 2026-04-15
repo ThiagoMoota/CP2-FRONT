4792b2c3f7325f6823d7abd7789c65550a99607c
Thiago Mota <thiago_mota123@hotmail.com>
2026-04-15 16:41:03 -0300

    Adicionando estrutura básica do APP

---
+++ checkpoint2/app/cadastro.tsx
@@ -0,0 +1,9 @@
+import { MaskedTextInput } from 'react-native-mask-text'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function Cadastro(){
+    return(
+
+    );
+}
